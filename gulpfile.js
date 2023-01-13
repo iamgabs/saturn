ece50555cb44d025c74d091a0b96ec5d8f36b9c9
iamgabs <anjomito26@gmail.com>
2023-01-12 23:50:14 -0300

    01/12/2023 - update files and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
-"use strict";
-
 const { dest, series, src, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function buildStyles() {;
+function buildSaturnSSStylesAndThemes() {;
     return src('src/saturn/**/*.scss')
-        .pipe(sass()) // -> compile 
+        .pipe(sass()) 
         .pipe(dest('src/css'))
         && 
         src('src/saturn/themes/**/*.scss')
-        .pipe(sass()) // -> compile 
+        .pipe(sass()) 
         .pipe(dest('src/css/themes/'));
 }
 
-function watchRepository() {
-    watch(['src/saturn/**/*.scss'], buildStyles);
+function watchAllScssFilesInTheRepository() {
+    watch(['src/saturn/**/*.scss'], buildSaturnSSStylesAndThemes);
 }
 
-exports.default = series(buildStyles, watchRepository)
+exports.default = series(buildSaturnSSStylesAndThemes, watchAllScssFilesInTheRepository)
